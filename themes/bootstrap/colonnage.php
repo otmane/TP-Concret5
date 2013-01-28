9481d1a5e5acc182b04e84768c76a062bfe9c40d
Otmane OUYAHIA <otmane.ouyahia@gmail.com>
2013-01-28 23:26:20 +0100

    Création de la page colonnage.php

---
+++ themes/bootstrap/colonnage.php
@@ -0,0 +1,37 @@
+<?php $this->inc('elements/header.php'); ?>
+		
+		   
+    
+		<div class="row">
+		    <div class="span4">
+		        
+				<div class="hero-unit">
+				 
+				</div>
+		
+			</div>
+			 <div class="span4">
+		       <div class="hero-unit">
+				 
+				</div>
+			</div>
+			 <div class="span4">
+		        <div class="hero-unit">
+				
+				</div>
+			</div>
+		</div>
+		<div class="row">
+		    <div class="span12">
+		       <div class="hero-unit">
+				
+				</div>
+			</div>
+			 
+		</div>
+
+			
+		      
+			
+				
+<?php $this->inc('elements/footer.php'); ?>
