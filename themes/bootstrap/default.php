fb88c9e6cbcc1002e1f153ff9adde48ca9b3799e
Otmane OUYAHIA <otmane.ouyahia@gmail.com>
2013-01-28 23:07:35 +0100

    commantaires sur default.php

--- themes/bootstrap/default.php
+++ themes/bootstrap/default.php
@@ -3,8 +3,9 @@
 		    <div class="container">
     
 		<div class="row">
+								<!-- Création d'une Colonne (Contenu) de 10  !-->
 		    <div class="span10">                            
-	                                        <!-- Création d'une Colone (Contenu) de 10  !-->
+	                              
 		        <div class="hero-unit">
 				 <?php
 				$a=new Area ('contenu');
@@ -13,13 +14,17 @@
 				</div>
 		      
 		      <div class="row">
-		      	<div class="span5">			<!-- Création d'une Colone (bas gauche) de 5  !-->
+								<!-- Création d'une Colonne (bas gauche) de 5  !-->
+		      	<div class="span5"> 
+				                   
 		      		<?php
 				$a=new Area ('bas gauche');
 				$a->display ($c);
 				?>
 		      	</div>	      	
-		      	<div class="span5">		    <!-- Création d'une Colone (bas droite) de 5  !-->
+								<!-- Création d'une Colonne (bas droite) de 5  !-->
+		      	<div class="span5">		    
+				                    
 		      		<?php
 				$a=new Area ('bas droite');
 				$a->display ($c);
@@ -28,7 +33,7 @@
 		      </div>
 		      
 		    </div>
-		    <div class="span2">				<!-- Création d'une Colone (lateral) de 12  !-->
+		    <div class="span2">				<!-- Création d'une Colonne (lateral) de 12  !-->
 		    	<div class="well well-small">
 					<?php
 				$a=new Area ('lateral');
