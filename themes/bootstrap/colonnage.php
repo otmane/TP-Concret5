4d76c75029ac27fbc5c48e5b2354c4a5b055951c
Otmane OUYAHIA <otmane.ouyahia@gmail.com>
2013-01-28 23:30:57 +0100

    Modification1 sur la page Colonnage.php

--- themes/bootstrap/colonnage.php
+++ themes/bootstrap/colonnage.php
@@ -6,25 +6,37 @@
 		    <div class="span4">
 		        
 				<div class="hero-unit">
-				 
+				 <?php
+				 $a = new Area('colonne 1');
+				  $a->display($c);
+				  ?>
 				</div>
 		
 			</div>
 			 <div class="span4">
 		       <div class="hero-unit">
-				 
+				 <?php
+				 $a = new Area('colonne 2');
+				  $a->display($c);
+				  ?>
 				</div>
 			</div>
 			 <div class="span4">
 		        <div class="hero-unit">
-				
+				 <?php
+				 $a = new Area('colonne 3');
+				  $a->display($c);
+				  ?>
 				</div>
 			</div>
 		</div>
 		<div class="row">
 		    <div class="span12">
 		       <div class="hero-unit">
-				
+				 <?php
+				 $a = new Area('colonne bas ');
+				  $a->display($c);
+				  ?>
 				</div>
 			</div>
 			 
